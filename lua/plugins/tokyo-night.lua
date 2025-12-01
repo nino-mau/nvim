56590abe9c365f7b1398648159b521d52a5647a1
nino.mauuary <nino.mauuary@gmail.com>
2025-12-02 00:43:38 +0100

    :sparkles:feat: change color of comment in tokyo night theme for better visibility with transparent background

--- lua/plugins/tokyo-night.lua
+++ lua/plugins/tokyo-night.lua
@@ -2,7 +2,11 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+  -- sdf sdfsdf
   opts = {
     transparent = true,
+    on_colors = function(colors)
+      colors.comment = "#8690AF"
+    end,
   },
 }
